9dec70022e179e301520bff889672bca4d861585
MAEDA Go <maeda@farend.jp>
2013-09-19 22:59:32 +0900

    画面表示が日本語のときはヘルプのリンク先をRemdine.JPの日本語訳にする

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -3,4 +3,7 @@ $(function(){
   $("a.external").attr("target","_blank");
   $("a.help").attr("target","_blank");
   $("div#footer a[href^='http://www.redmine.org/']").attr("target","_blank");
+
+  /* 言語が日本語のときはヘルプのリンク先をRedmine.JPの日本語訳にする */
+  $("html[lang='ja'] a.help").attr("href", "http://redmine.jp/guide/");
 });
