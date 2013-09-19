ed62976a15cff15cebf27cf3d2d5aa16cee6adbd
MAEDA Go <maeda@farend.jp>
2013-09-19 22:53:24 +0900

    ヘルプおよびフッタ部のオフィシャルサイトへのリンクを新規ウィンドウで開くよう変更。

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,4 +1,6 @@
-/* 外部リンクは新規ウィンドウで開く */
 $(function(){
+  /* 外部リンクは新規ウィンドウで開く */
   $("a.external").attr("target","_blank");
+  $("a.help").attr("target","_blank");
+  $("div#footer a[href^='http://www.redmine.org/']").attr("target","_blank");
 });
