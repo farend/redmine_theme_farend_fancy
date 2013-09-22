938addd34c2af4b6d5961e662f6f9dc857dd71f2
MAEDA Go <maeda@farend.jp>
2013-09-22 17:09:32 +0900

    検索におけるスコープを検索ボックスのプレースホルダーに表示

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -4,6 +4,30 @@ $(function(){
   $("a.help").attr("target","_blank");
   $("div#footer a[href^='http://www.redmine.org/']").attr("target","_blank");
 
-  /* 言語が日本語のときはヘルプのリンク先をRedmine.JPの日本語訳にする */
-  $("html[lang='ja'] a.help").attr("href", "http://redmine.jp/guide/");
+
+  var lang = $("html").attr("lang");
+  if (lang == "ja") {
+    /* 言語が日本語のときはヘルプのリンク先をRedmine.JPの日本語訳にする */
+    $("a.help").attr("href", "http://redmine.jp/guide/");
+
+    /* 検索におけるスコープを検索ボックスのプレースホルダーに表示 */
+    $("body").attr("class").match(/controller-[\S]+/);
+    var controllerName = RegExp.lastMatch;
+    var placeholderText = "Redmine内を検索"
+    var placeholderStrings = [
+      {controller: "controller-issues", text: "チケットを検索"},
+      {controller: "controller-news", text: "ニュースを検索"},
+      {controller: "controller-documents", text: "文書を検索"},
+      {controller: "controller-changesets", text: "更新履歴を検索"},
+      {controller: "controller-wiki_pages", text: "Wikiページを検索"},
+      {controller: "controller-messages", text: "メッセージを検索"}
+    ];
+    jQuery.each(placeholderStrings, function() {
+      if (controllerName === this.controller) {
+      	placeholderText = this.text;
+      	return false;
+      }
+    });
+    $("#quick-search input#q").attr("placeholder", placeholderText);
+  }
 });
